2a6ef532de21b0c0ed6bdffddcb0219d3342ae47
developer <developer@example.com>
2024-07-10 12:04:48 +0300

    Bootstrap

--- js/gallery-helper.js
+++ js/gallery-helper.js
@@ -1,15 +1,17 @@
 export const toGalleryEntry = entry => {
   const displayEntry = document.createElement("a");
-  displayEntry.className = 'gallery';
+  displayEntry.className = 'card col';
+  displayEntry.target = '_blank';
   displayEntry.href = `https://wikipedia.org/wiki/${entry.itemLabel.value}`
 
-  const description = document.createElement("div");
-  description.className = 'desc';
+  const description = document.createElement("h5");
+  description.className = 'card-title';
   description.textContent = entry.itemLabel.value;
   displayEntry.appendChild(description);
 
   const image = document.createElement("img");
   const imageLink = entry.itemImageSample?.value;
+  image.className = 'card-img-bottom';
   image.src = imageLink ? imageLink : "img/icon-image-not-found.jpg";
   displayEntry.appendChild(image);
 
