dedec5042bee3982eb09c2a57251219677918f2e
developer <developer@example.com>
2024-07-10 12:04:49 +0300

    Somewhat usable dropdown

--- js/filters.js
+++ js/filters.js
@@ -6,22 +6,24 @@ export const prepareFilters = () => Object.entries(mapping).forEach(
     if (select) {
       const options = Object.entries(entry[1])
         .map(option =>
-          `<li id="${option[0]}"><a href="#">
+          `<div id="${option[0]}" class="col"><a href="#">
             <img class="img-thumbnail" src="img/${option[0]}_icon.png" width="100px" alt="${option[0]}"
                 data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="${option[0]}"/>
-           </a></li>`
+           </a></div>`
         )
         .join('');
 
       select.innerHTML = `
       <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="${entry[0]}-button">${entry[0]}</button>
-      <ul class="dropdown-menu">${options}</ul>
+      <div class="dropdown-menu">
+        <div class="row row-cols-2">${options}</div>
+      </div>
   `
       select.addEventListener("hide.bs.dropdown", (event) => {
         const parent = event.clickEvent.srcElement.parentElement;
         const id = parent.id ? parent.id : parent.parentElement.id;
         document.getElementById(`${entry[0]}-button`).innerHTML =
-          `<img class="img-thumbnail"  src="img/${id}_icon.png" width="100px" alt="${id}"/>`;
+          `<img class="img-thumbnail" src="img/${id}_icon.png" width="100px" alt="${id}"/>`;
       });
     }
   }
